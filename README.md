90b939dfe1356f83d5b534a4f7235355c86957a0
Kayllane <pinakayllane@gmail.com>
2021-10-11 11:39:30 -0300

    adiciona os codigos

---
+++ README.md
@@ -0,0 +1,21 @@
+# Calculadora IMC com Node.JS
+
+****************
+
+### O que aprendi:
+
+* O que é API
+* Receber requisições no back-end com Express
+* O que são parâmetros
+* Tratar os parâmetros na API
+* Refatorar o código
+* Retornar um JSON com mais de uma chave
+
+
+
+*Curso de Node.JS da plataforma DevMedia*
+
+
+
+
+
---
+++ index.js
@@ -0,0 +1,22 @@
+// Tratando os parâmetros na API
+const express = require('express');
+const app = express();
+
+const calculadoraIMC = require('./calculadoraImc');
+
+app.get('/', (req, res) => {
+    let peso = req.query.peso;
+    let altura = req.query.altura;
+
+    let imc =  calculadoraIMC.efetuarCalculoIMC(peso, altura);
+    let status = calculadoraIMC.retornaStatusIMC(imc);
+
+    let json = {imc: imc, status: status};
+
+    res.json(json);
+});
+
+app.listen(8080, () =>{
+  let data = new Date();
+  console.log(`Servidor node iniciado em ${data}`);
+});
